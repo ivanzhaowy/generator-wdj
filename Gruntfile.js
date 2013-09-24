098f1de626284ab4f2f4a4b16e35f100aebcb6b2
Wangye Zhao <wangye.zhao@wandoujia.com>
2013-09-24 18:46:16 +0800

    Add bump script.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // configurable paths
+    var pathConfig = {
+        app : 'app',
+        dist : 'dist',
+        tmp : '.tmp'
+    };
+
+    grunt.initConfig({
+        paths : pathConfig,
+        bump : {
+            options : {
+                files : ['package.json'],
+                updateConfigs : [],
+                commit : true,
+                commitMessage : 'Release v%VERSION%',
+                commitFiles : ['-a'],
+                createTag : true,
+                tagName : 'v%VERSION%',
+                tagMessage : 'Version %VERSION%',
+                push : false
+            }
+        }
+    });
+
+    grunt.registerTask('release:patch', [
+        'bump:patch'
+    ]);
+};
