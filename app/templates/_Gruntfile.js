b9021337a06daddc80148a6c3f4e7b3e8da5db7b
Wangye Zhao <wangye.zhao@wandoujia.com>
2014-11-20 12:00:22 +0800

    feat: remove `console`, `debugger` and unreachable

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -201,11 +201,23 @@ module.exports = function (grunt) {
                     uglify: {
                         toplevel: true,
                         ascii_only: false,
-                        beautify: false
+                        beautify: false,
+                        drop_console: true,
+                        drop_debugger: true,
+                        dead_code: true
                     },
                     preserveLicenseComments: true,
                     useStrict: false,
                     wrap: true
+                }
+            }
+        },
+        uglify: {
+            options: {
+                compress: {
+                    dead_code: true,
+                    drop_console: true,
+                    drop_debugger: true
                 }
             }
         },
