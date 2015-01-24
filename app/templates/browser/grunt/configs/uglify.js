91389f0f048183780a65408d5b0cf4f5c800c34a
Wangye Zhao <wangye.zhao@wandoujia.com>
2015-01-24 18:40:08 +0800

    feat: Add banner to .js files

--- app/templates/browser/grunt/configs/uglify.js
+++ app/templates/browser/grunt/configs/uglify.js
@@ -1,6 +1,9 @@
 module.exports = function (grunt) {
     grunt.config.set('uglify', {
         options: {
+            banner: grunt.file.read(__dirname + '/banner.txt'),
+            sourceMap: true,
+            preserveComments: 'some',
             compress: {
                 drop_console: true,
                 drop_debugger: true
